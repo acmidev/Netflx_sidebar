0db215b383c6ff0cee33c5df23296fe878764f87
acmidev <mharmel.ach@gmail.com>
2022-03-03 16:46:33 +0100

    added SPA functionality

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),            
+            new Route('browse', 'browse.html'),
+            new Route('watchlist', 'watchlist.html'),
+            new Route('friends', 'friends.html'),
+            new Route('notifications', 'notifications.html'),
+            new Route('tv-shows', 'tv-shows.html'),
+            new Route('movies', 'movies.html'),
+            new Route('games', 'games.html'),
+            new Route('settings', 'settings.html'),
+            new Route('log-out', 'log-out.html')
+           
+        ]);
+    }
+    init();
+}());
